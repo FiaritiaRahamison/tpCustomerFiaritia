43c43a3aba07db550dbb316c9404457250e922b0
Fiaritia <fiaritia-naliarilala.rahamison@etu.unice.fr>
2024-04-01 04:28:30 +0300

    Ajout de boutons pour la mise à jour et retour à la liste.

--- src/main/java/mg/fiaritia/tpcustomerfiaritia/service/CustomerManager.java
+++ src/main/java/mg/fiaritia/tpcustomerfiaritia/service/CustomerManager.java
@@ -14,27 +14,28 @@ import mg.fiaritia.tpcustomerfiaritia.entity.Customer;
 
 /**
  * Façade pour gérer les Customers.
+ *
  * @author raham
  */
 @RequestScoped
 public class CustomerManager {
-    
+
     @PersistenceContext(unitName = "fiaritiaCustomerPU")
     private EntityManager em;
 
     /**
-     * Récuperer la liste des Customers :
-     * la requête correspond à un "select *"
+     * Récuperer la liste des Customers : la requête correspond à un "select *"
+     *
      * @return List
      */
-    public List<Customer> getAllCustomers() {  
+    public List<Customer> getAllCustomers() {
         Query query = em.createNamedQuery("Customer.findAll");
         return query.getResultList();
-    }  
+    }
 
     /**
-     * Modification d'un customer
-     * Génèrera un UPDATE SQL
+     * Modification d'un customer Génèrera un UPDATE SQL
+     *
      * @param customer
      * @return Customer
      */
@@ -42,14 +43,24 @@ public class CustomerManager {
     public Customer update(Customer customer) {
         return em.merge(customer);
     }
-    
+
     /**
-     * Fait une nouvelle insertion:
-     * génèrera un INSERT SQL dans la base de données
-     * @param customer 
+     * Fait une nouvelle insertion: génèrera un INSERT SQL dans la base de
+     * données
+     *
+     * @param customer
      */
     @Transactional
     public void persist(Customer customer) {
         em.persist(customer);
+    }
+
+    /**
+     * Rechercher un customer suivant son identifiant
+     * @param idCustomer
+     * @return Customer
+     */
+    public Customer findById(int idCustomer) {
+        return em.find(Customer.class, idCustomer);
     }
 }
